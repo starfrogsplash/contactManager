d9ee83584c71b858e46f1eeb505a9e52c4e6a1ec
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-11-30 15:32:15 +0000

    Updates

---
+++ precache-manifest.777429bd41488fa27374d04f315216c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9c7142bbfeec04feca1",
+    "url": "/contactManager/static/js/runtime~main.c9c7142b.js"
+  },
+  {
+    "revision": "24229314b2f8be1205a0",
+    "url": "/contactManager/static/js/main.24229314.chunk.js"
+  },
+  {
+    "revision": "b3b3c705e525aa7ffa7f",
+    "url": "/contactManager/static/js/1.b3b3c705.chunk.js"
+  },
+  {
+    "revision": "24229314b2f8be1205a0",
+    "url": "/contactManager/static/css/main.844a8e65.chunk.css"
+  },
+  {
+    "revision": "b3b3c705e525aa7ffa7f",
+    "url": "/contactManager/static/css/1.3f54db14.chunk.css"
+  },
+  {
+    "revision": "99e7867889d0de19d81100068cfcb735",
+    "url": "/contactManager/index.html"
+  }
+];
