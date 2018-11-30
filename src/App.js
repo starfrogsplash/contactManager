62f311f16dcc726cb434219d63fa501bc31f3bb7
Ali Khalil <ali_khalil@hotmail.co.uk>
2018-11-30 15:45:01 +0000

    changed from hashrouter to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Contacts from './components/contacts/Contacts'
 import Header from './components/layout/Header'
@@ -15,7 +15,7 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div className="App">
                 <Header branding="Content Manager"/>  
                 <div className="container">
